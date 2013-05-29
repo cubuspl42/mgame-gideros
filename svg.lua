3c01ec018cbb48522df0b863415c26b4194ea654
cubuspl42 <cubuspl42@tesign.pl>
2013-05-29 22:23:28 +0200

    added triangulation.lua

--- svg.lua
+++ svg.lua
@@ -1,37 +1,23 @@
 SVG = {}
 
 function SVG.parseStyle(styleString) -- return table
-    
+
 end
 
 function SVG.pathToVertices(pathTag) -- <path d="defString" />
-    
+
 end
 
 function SVG.rectToVertices(rectTag) 
-    
+
 end
 
 function SVG.pathDefToVertices(d)
-    
+
 end
 
-local function cross(vx, vy, ovx, ovy)
-    return vx * ovy - vy * ovx
-end
-
-function SVG.triangulatePolygon(vertices) -- {{x,y}...}
-    local n = numVertices(vertices)
-    local ret = {}
-    while #vertices > 3 do
-        for i, vertex in ipairs(vertices) do
-            local x, y = vertex[0], vertex[1]
-            local j = (i + 2) % #vertices
-            local x1, y1 = vertices[j][0], vertices[j][1]
-            local vx, vy = x1 - x, y1 - y
-            
-        end
-    end
+function SVG.triangulatePolygon(vertices) -- {{x=X,y=Y}...}
+	return delaunay(vertices)
 end
 
 
