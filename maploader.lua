cea5ed420260bacfe51503a72b9d8550be95d102
cubuspl42 <cubuspl42@tesign.pl>
2013-06-03 17:03:30 +0200

    added maploader
    which will use msvg in close future

--- maploader.lua
+++ maploader.lua
@@ -1,5 +1,53 @@
 MapLoader = {}
+local tag = {}
+
+local function visitTag(p, t)
+	if tag[t:name()] then
+		local item = tag[t:name()](t)
+		if item then	
+			p:addChild(item)
+		end
+	end
+	
+end
+
+function tag.g(t)
+	local g = Sprite.new()
+	local transform = t['@transform']
+	print("transform = ", transform)
+	if transform then
+		local global = preserve("matrix", "translate")
+	
+		function matrix(...)
+			print("matrix called with ", ...)
+			g:setMatrix(Matrix.new(...))
+		end
+	
+		function translate(...)
+			print("translate called with ", ...)
+			g:setPosition(...)
+		end
+		
+		local chunk = loadstring(transform)
+		if chunk then
+			chunk()
+		end
+	
+		revert(global)
+	end
+	for i, child in ipairs(t:children()) do
+		visitTag(g, child)
+	end
+end
+
+function tag.path(t)
+	print "visit path"
+end
 
 function MapLoader.loadMap(svgTag)
-    tprint(svgTag, 1)
+	local mapLayer = Sprite.new()
+    for i, child in ipairs(svgTag:children()) do
+		visitTag(mapLayer, child)
+	end
+	return mapLayer
 end
